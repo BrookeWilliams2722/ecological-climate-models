c51809ee03a1a393059bb5753596780e612ea8a6
Jonathan Rhodes <j.rhodes@uq.edu.au>
2020-11-06 16:58:16 +1000

    Initial code for data import

--- tree_models.r
+++ tree_models.r
@@ -1,12 +1,15 @@
 # load required libraries
 library(tidyverse)
-library(unmarked)
-library(MASS)
 library(parallel)
-library(extraDistr)
-library(binom)
+library(gdal)
+library(raster)
+library(sp)
 
 # location of functions
-source("functions.r")
+#source("functions.r")
 
 # load tree data
+TreeData <- read_csv(file = "input/tree_data_from_jill.csv")
+
+# load species list
+Species <- read_csv(file = "input/species_list_from_jill.csv")
