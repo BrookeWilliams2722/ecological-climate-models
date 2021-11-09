33057b1a83ff6aa7102aec6b6ac8abcbcbf9fac0
jara <j.villarrealrosas@uq.edu.au>
2021-11-09 17:19:54 +1000

    preparing data

--- maxent.R
+++ maxent.R
@@ -1,6 +1,10 @@
 library(raster)
+library(sf)
+library(dismo)
+library(tidyverse)
 
-# read baseline data
+
+# read baseline data (predictors)  
 drx <- "data/baseline"
 files.baseline <- list.files(path = drx, pattern = "*.tif$", full.names = TRUE)
 
@@ -10,5 +14,16 @@ for(i in 1:length(files.baseline)) {
   baseline <- stack(files.baseline[i], baseline)
 }
 
-# testing this 
-# test number two
+# read the occurrence data 
+
+occurrence_NSW <- read_sf(dsn = "data", layer = "KoalaRecords_1979_2019_FINAL_40005") %>% 
+                  select(SF_06) %>% 
+                  subset(SF_06 == 1)
+
+coordinates <- st_coordinates(occurrence_NSW)
+
+coordinates <- as.data.frame(coordinates)
+
+
+# next -> background points
+# next -> run model 
