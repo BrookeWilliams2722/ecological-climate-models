61caa165174a6818df906350a55bf3eb7b186c79
jara <j.villarrealrosas@uq.edu.au>
2021-11-15 19:32:03 +1000

    baseline tuned and no tuned

--- maxent.R
+++ maxent.R
@@ -2,9 +2,16 @@ library(raster)
 library(sf)
 library(dismo)
 library(tidyverse)
+library(terra)
 
+# Read Baseline Data (predictors)  
+# Linda used 5 predictors:
+  # o	Maximum temperature of the warmest month (BIO5) 
+  # o	Annual precipitation (BIO12) 
+  # o	Soil pH 
+  # o	Soil organic carbon 
+  # o	Available water capacity 
 
-# read baseline data (predictors)  
 drx <- "data/baseline"
 files.baseline <- list.files(path = drx, pattern = "*.tif$", full.names = TRUE)
 
@@ -14,23 +21,116 @@ for(i in 1:length(files.baseline)) {
   baseline <- stack(files.baseline[i], baseline)
 }
 
-# read the occurrence data 
+# Read Occurrence Data 
 
 occurrence_NSW <- read_sf(dsn = "data", layer = "KoalaRecords_1979_2019_FINAL_40005") %>% 
                   select(SF_06) %>% 
                   subset(SF_06 == 1)
+occ <- st_coordinates(occurrence_NSW)
+occ <- as.data.frame(occ)
 
-coordinates <- st_coordinates(occurrence_NSW)
+# Sample Random Background Points, 
+# selected 50000 following Valavi et al 2021 and Linda's model  
 
-coordinates <- as.data.frame(coordinates)
+bground <- randomPoints(baseline, 50000, p = occ, 
+                        ext=NULL, extf=1.1, excludep=TRUE, prob=FALSE, 
+                        cellnumbers=FALSE, tryf=3, warn=2, lonlatCorrection=TRUE) %>% 
+                        as_tibble() %>% 
+                        mutate(occ = 0) 
+names(bground) <- c("X", "Y", "occ")
 
-# witholding a 20% sample for testing 
-fold <- kfold(coordinates, k=5)
-coordinatestest <- coordinates[fold == 1, ]
-coordinatestrain <- coordinates[fold != 1, ]
+# Witholding a 20% sample for testing, 80% for training 
 
-# fit model using 50000 random background points
+fold <- kfold(occ, k=5)
+occtest <- occ[fold == 1, ]
+occtrain <- occ[fold != 1, ]
 
-baseline_maxent <- maxent(baseline, coordinatestest, nbg = 50000, 
-                          path = "S://Jaramar//Otros proyectos academicos_ultima_version//RA_Koala//koala_Maxent//maxent_koalas_R//output//baseline")
+# Combine Presence (training) and Background Points
+train_bground <- bind_rows(occtrain %>% mutate(occ = 1), bground)
+
+# Fixing Problems with memory
+
+memory.size() ### Checking your memory size
+memory.limit() ## Checking the set limit
+memory.limit(size=56000) # Assign memory limit 
+
+# Extract environmental data at each occurrence
+covars_train <- terra::extract(baseline,
+                         train_bground %>% dplyr::select(-occ),
+                         method = "bilinear", 
+                         list = FALSE) %>% 
+                        as.data.frame()
+
+# Fit a MaxEnt model with the training parameters NO TUNNING
+maxent_notunning_baseline <- dismo::maxent(x = covars_train,
+                                   p = train_bground$occ,
+                                   path = "output/maxent_baseline_notunning_50000")
+                                   
+save(maxent_notunning_baseline, file = "output/maxent_baseline_notunning_50000/maxent_notunning_baseline.RData")
+
+# Predict to the entire of NSW 
+
+predictions_notunn <- predict(maxent_notunning_baseline, baseline)
+save(predictions_notunn, file = "output/maxent_baseline_notunning_50000/prediction_notunning_baseline.RData")
+
+# Compare observed data to model data
+
+# Evaluate and Find the right threshold
+eval_no_tunning <- evaluate(p = occtest,
+                 a = bground %>% 
+                   dplyr::select(X,Y),
+                 model = maxent_notunning_baseline, 
+                 x = baseline) 
+
+thresh_no_tunning <- threshold(eval_no_tunning)
+
+# Convert predictions into binary
+pred_binary_notunn <- overlay(predictions_notunn, 
+                             fun = function(x){
+                               ifelse(x <=thresh_no_tunning$spec_sens,NA,1)})
+
+save(pred_binary_notunn, file = "output/maxent_baseline_notunning_50000/pred_binary_notunn.RData")
+
+
+# TUNNING MaxEnt, using maxent_param function from Valavi et al 2021 ----
+
+nfolds <- ifelse(sum(occtrain$occ) < 10, 2, 5) 
+
+param_optim_50000 <- maxent_param(data = train_bground,
+                                  k = nfolds,
+                                  filepath = "output/maxent_param_50000")
+
+# Fit a MaxEnt model with the training parameters TUNNING
+maxent_tunning_baseline <- dismo::maxent(x = covars_train,
+                                           p = train_bground$occ,
+                                           args = param_optim_50000,
+                                           path = "output/maxent_baseline_tunning_50000")
+
+save(maxent_tunning_baseline, file = "output/maxent_baseline_tunning_50000/maxent_tunning_baseline.RData")
+
+# Predict to the entire of NSW 
+
+predictions_tunned <- predict(maxent_tunning_baseline, baseline)
+save(predictions_tunned, file = "output/maxent_baseline_tunning_50000/predictions_tunned.RData")
+
+# Compare observed data to model data
+
+# Find the right threshold
+
+eval_tunning <- evaluate(p = occtest,
+                            a = bground %>% 
+                              dplyr::select(X,Y),
+                            model = maxent_tunning_baseline, 
+                            x = baseline) 
+
+thresh_tunning <- threshold(eval_tunning)
+
+# Convert model data into binary
+
+pred_binary_tunn <- overlay(predictions_tunned, 
+                             fun = function(x){
+                               ifelse(x <=thresh_tunning$spec_sens,NA,1)}) 
+
+save(pred_binary_tunn, file = "output/maxent_baseline_tunning_50000/pred_binary_tunn.RData")
+
 
